795b079cdd1aee1529850ab7ffc1a99f6976a4ba
jmarshalltech <60329368+jmarshalltech@users.noreply.github.com>
2021-09-23 18:28:42 +0100

    Changed offset for CurrentMarker

--- src/pages/Landing/StakingRewardsChart/scene/CurrentMarker.tsx
+++ src/pages/Landing/StakingRewardsChart/scene/CurrentMarker.tsx
@@ -7,12 +7,12 @@ interface Props {
 }
 
 const CurrentMarker: React.FC<Props> = ({ x, y }) => (
-  <g transform={`translate(${x - 15.5}, ${y - 22})`}>
+  <g transform={`translate(${x - 22.8}, ${y - 26.5})`}>
     <g
       id="Hexagon"
       transform="translate(23.000000, 27.000000) rotate(-90.000000) translate(-23.000000, -27.000000) translate(-2.000000, 5.000000)"
-      fill-rule="nonzero"
       stroke="#1C2D54"
+      fill="white"
       stroke-width="2"
     >
       <polygon points="50 22 37.5 44 12.5 44 0 22 12.5 -1.28173664e-14 37.5 -1.28173664e-14"></polygon>
