52dc42cc3b0ec97d842c454e0aec16ec0be18ddb
Triex <alex@ruffbeatz.com>
2021-09-24 17:15:00 +1000

    Adjust TOS, Remove merge ref in advisories

--- src/pages/Acknowledgements/pageContent.tsx
+++ src/pages/Acknowledgements/pageContent.tsx
@@ -192,13 +192,6 @@ export const pageContent = {
               withdraw or transfer real tokens.`}
           />
         </Text>
-        <Link
-          to="https://ethereum.org/en/eth2/docking"
-          className="my10"
-          primary
-        >
-          <FormattedMessage defaultMessage="More on the merge" />
-        </Link>
         <Text size="medium" className="my10">
           <FormattedMessage
             defaultMessage="With transfers disabled for now, you won't be able to voluntarily exit
