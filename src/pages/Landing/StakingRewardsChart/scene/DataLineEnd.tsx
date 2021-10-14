4be0c9b9d743053fffc321da020f292bd565ca6f
jmarshalltech <james@jmarshall.uk>
2021-10-14 16:45:00 +0100

    Changed staking graph to lukso icons

--- src/pages/Landing/StakingRewardsChart/scene/DataLineEnd.tsx
+++ src/pages/Landing/StakingRewardsChart/scene/DataLineEnd.tsx
@@ -6,14 +6,13 @@ interface Props {
   color?: string;
 }
 
-const DataLineEnd: React.FC<Props> = ({ x, y, color = '#045388' }) => (
-  <g transform={`translate(${x - 9.5}, ${y - 12})`}>
-    <path
-      d="M15.663 11.1655L11.6424 4.27007L9.91467 5.27749L11.6424 4.27007C10.4863 2.28736 7.62271 2.28432 6.46241 4.26456L2.42219 11.1598C1.82971 12.171 1.88029 13.4345 2.55169 14.3951L6.59201 20.1756C7.78781 21.8864 10.3219 21.8835 11.5137 20.1699L15.5342 14.3896C16.2007 13.4315 16.2509 12.1737 15.663 11.1655Z"
-      fill={color}
-      stroke={color}
-      strokeWidth="4"
-    />
+const DataLineEnd: React.FC<Props> = ({ x, y }) => (
+  <g transform={`translate(${x - 8}, ${y - 12})`}>
+    <g transform="translate(0.000000, 0.833333)" fill="#1C2D54">
+      <g transform="translate(10.000000, 11.666667) rotate(90.000000) translate(-10.000000, -11.666667) translate(-1.666667, 1.666667)">
+        <polygon points="23.3333333 10 17.5 20 5.83333333 20 0 10 5.83333333 0 17.5 0"></polygon>
+      </g>
+    </g>
   </g>
 );
 
--- src/pages/Landing/StakingRewardsChart/scene/EthMarker.tsx
+++ src/pages/Landing/StakingRewardsChart/scene/EthMarker.tsx
@@ -6,20 +6,33 @@ interface Props {
   color?: string;
 }
 
-const EthMarker: React.FC<Props> = ({ x, y, color = '#045388' }) => (
-  <g transform={`translate(${x - 15.5}, ${y - 22})`}>
-    <path
-      d="M28.0105 22.2916L18.3406 4.48692C17.2063 2.39842 14.2095 2.39535 13.071 4.48152L3.35378 22.286C2.82514 23.2546 2.87092 24.4353 3.47298 25.36L13.1903 40.2858C14.3752 42.1058 17.0413 42.1028 18.2222 40.2802L27.892 25.3546C28.4895 24.4323 28.535 23.2572 28.0105 22.2916Z"
-      fill={color}
-      stroke={color}
-      strokeWidth="4"
-    />
-    <path
-      d="M15.7008 26.0613L8.20605 21.3505M15.7008 26.0613L23.0522 21.3505M15.7008 26.0613V17.4248M8.20605 21.3505L15.7008 8.50281M8.20605 21.3505L15.7008 17.4248M15.7008 8.50281L23.0522 21.3505M15.7008 8.50281V17.4248M23.0522 21.3505L15.7008 17.4248M15.6297 29.9885L8.18799 25.383L15.6297 36.6265M15.6297 29.9885L23.1722 25.3829L15.6297 36.6265M15.6297 29.9885V36.6265"
-      stroke="white"
-      strokeWidth="1.5"
-      strokeLinejoin="round"
-    />
+const EthMarker: React.FC<Props> = ({ x, y }) => (
+  <g transform={`translate(${x - 22}, ${y - 25})`}>
+    <g
+      id="Hexagon"
+      transform="translate(22.000000, 25.000000) rotate(-90.000000) translate(-22.000000, -25.000000) translate(-3.000000, 3.000000)"
+      fill="#1C2D54"
+      fill-rule="nonzero"
+    >
+      <polygon points="50 22 37.5 44 12.5 44 0 22 12.5 -1.28173664e-14 37.5 -1.28173664e-14"></polygon>
+    </g>
+    <g id="Group" transform="translate(11.000000, 15.000000)">
+      <polygon
+        id="Front"
+        fill="#DBD3D3"
+        points="0 10 5.729799 20 17 20 11.3282072 10"
+      ></polygon>
+      <polygon
+        id="Top"
+        fill="#EEEDED"
+        points="0 10 5.71455551 4.67494424e-16 17 0 11.3282072 10"
+      ></polygon>
+      <polygon
+        id="Right"
+        fill="#E7E7E7"
+        points="16.9927665 0 23 9.98922893 16.9927665 20 11 10"
+      ></polygon>
+    </g>
   </g>
 );
 
